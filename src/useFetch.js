ebecc6ad5f19c80e7a29a3f429aea06932a8d105
Samsuddin-Shohan <shohan.cse.hstu@gmail.com>
2021-12-08 04:19:14 +0600

    blog finalized

---
+++ src/useFetch.js
@@ -0,0 +1,46 @@
+
+import {useEffect, useState} from 'react';
+
+const useFetch = ((url) => {
+    const [data, setData] = useState(null);  
+    //    const [name, setName] = useState('mario');
+        const [isPending, setIsPending] = useState(true)
+        const [error, setError] = useState(null)
+    useEffect(() => {
+        //  console.log('useeffect just ran');
+         // console.log(name);
+         const abortCont = new AbortController();
+         setTimeout(() =>{
+           fetch(url, {signal : abortCont.signal})
+           .then(res => {
+              if(!res.ok){
+                  throw Error("can't fetch data from database");
+               }
+               return res.json();
+           })
+           .then(data => {
+               setData(data);
+               setIsPending(false);
+               setError(null);
+           })
+           .catch(err => {
+               if(err.name ==='AbortError'){
+                   console.log('fetch aborted');
+               }
+               else{
+                setIsPending(false);
+                setError(err.message);
+
+
+               }
+           })
+         } , 1000)
+         return () => abortCont.abort();
+         
+         
+       }, [url]);
+       return{data, isPending, error};
+
+})
+
+export default useFetch;
